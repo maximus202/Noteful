0b719fa84310ee184caa18d710420208eb83e4dd
Max R <maxx.ratto@gmail.com>
2020-01-09 15:02:41 -0700

    new commit

--- src/AddNoteForm/AddNoteForm.js
+++ src/AddNoteForm/AddNoteForm.js
@@ -6,14 +6,39 @@ import GenerateFolderRadioButton from '../GenerateFolderRadioButton/GenerateFold
 class AddNoteForm extends React.Component {
     constructor() {
         super()
+
         this.state = {
             name: '',
             folderId: '',
             content: ''
         }
+        this.handleNameChange = this.handleNameChange.bind(this)
+        this.handleContentChange = this.handleContentChange.bind(this)
+        this.handleFolderIdChange = this.handleFolderIdChange.bind(this)
     }
 
+    handleNameChange(event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
 
+    handleContentChange(event) {
+        this.setState({
+            content: event.target.value
+        })
+    }
+
+    handleFolderIdChange(event) {
+        this.setState({
+            folderId: event.target.value
+        })
+    }
+
+    handleSubmitNote(e) {
+        e.preventDefault();
+        console.log(e)
+    }
 
     render() {
         return (
@@ -26,11 +51,11 @@ class AddNoteForm extends React.Component {
                                 <Header />
                             </header>
                             <main>
-                                <form>
+                                <form onSubmit={(e) => this.handleSubmitNote(e)}>
                                     <fieldset>
                                         <legend>Add Note</legend>
-                                        <input type='text' placeholder='Note title' name='name' required></input>
-                                        <input type='text' placeholder='Write your note here' name='note' required></input>
+                                        <input type='text' placeholder='Note title' name='name' ref={this.nameInput} onChange={this.handleNameChange} required></input>
+                                        <input type='text' placeholder='Write your note here' name='note' ref={this.contentInput} onChange={this.handleContentChange} required></input>
                                         {value.folders.map(folder =>
                                             <GenerateFolderRadioButton key={folder.id} folder={folder} />
                                         )}
